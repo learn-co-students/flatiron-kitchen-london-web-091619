29f921307590ab57b06f4c0ed467dea57ea5dd47
MARCO MCNULTY <Marco@MARCOs-MacBook-Pro-2.local>
2019-10-19 19:42:26 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # INGREDIENT ROUTES
+  get "/ingredients/", to: "ingredients#index", as: 'ingredients'
+  get "/ingredients/new", to: "ingredients#new", as: 'new_ingredient'
+  post "/ingredients/", to: "ingredients#create"
+  get "/ingredients/:id", to: "ingredients#show", as: 'ingredient'
+  get "/ingredients/:id/edit", to: "ingredients#edit", as: 'edit_ingredient'
+  patch "/ingredients/:id/", to: "ingredients#update"
+
+  # RECIPE ROUTES
+  get '/recipes/', to: 'recipes#index', as: 'recipes'
+  get "/recipes/new", to: "recipes#new", as: "new_recipe"
+  post "/recipes/", to: "recipes#create"
+  get "/recipes/:id", to: "recipes#show", as: "recipe"
+  get "/recipes/:id/edit", to: "recipes#edit", as: "edit_recipe"
+  patch "/recipes/:id/", to: "recipes#update"
+
 end
